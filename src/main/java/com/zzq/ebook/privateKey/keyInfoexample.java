cfe707e5581ce7019732a9baedd05afe0ec66710
Musicminion <zhangziqian@sjtu.edu.cn>
2022-06-14 21:35:24 +0800

    6-10 用户管理完成功能

--- src/main/java/com/zzq/ebook/privateKey/keyInfoexample.java
+++ src/main/java/com/zzq/ebook/privateKey/keyInfoexample.java
@@ -1,4 +1,10 @@
 package com.zzq.ebook.privateKey;
 
+// 原始的文件 keyinfo，为方便保密，不上传
+// 源文件名称 keyinfo.java
+
 public class keyInfoexample {
+    public static final String accessId = "阿里云accessId ";
+
+    public static final String picKey = "阿里云 key ";
 }
